8dc47d81c27f3a9457cdcbe896f1e7ab1b81b5c0
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-06-16 21:51:03 -0700

    add instance configuration scripts

---
+++ ansible_cloud_project/final/instance_config_scripts/nfs_ldap_client.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+apt-get update
+
+export DEBIAN_FRONTEND=noninteractive
+debconf_url='https://raw.githubusercontent.com/codycodes/Linux_at_SCC_NTI/master/NTI310/automagic_install/ldab_5_nfs_ldap/debconf'
+wget --no-verbose -P /tmp/ $debconf_url
+sed -i.bak "\,ldap://,s,$,$1," /tmp/debconf # add internal ip to debconf
+while read line; do echo "$line" | debconf-set-selections; done < /tmp/debconf # read each line as input to set debconf
+
+
+apt-get --yes install libpam-ldap nscd
+unset DEBIAN_FRONTEND
+
+sed -i.bak '/compat/s/$/ ldap/' /etc/nsswitch.conf # sed line to append ldap w/"compat" match
+systemctl reload-or-restart nscd
+
+# nfs client automation script
+apt-get install -y nfs-client
+mkdir /mnt/test
+echo "$2:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+mount -a
+cd /mnt/test/
